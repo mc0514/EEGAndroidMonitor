441075692028dc3ddb3b1244df3d26751e8e9bfa
= <=>
2016-03-29 19:27:32 +0800

    modified for the bug that x axis not changing by the data

--- android/EEG211/app/src/main/java/com/czu/chao/eeg211/Details.java
+++ android/EEG211/app/src/main/java/com/czu/chao/eeg211/Details.java
@@ -21,14 +21,20 @@ import android.widget.Toast;
 
 import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYStepMode;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.UUID;
 
@@ -64,12 +70,18 @@ public class Details extends AppCompatActivity implements View.OnClickListener {
         plot = (XYPlot) findViewById(R.id.ecgPlot);
 
         something = new SimpleXYSeries("EEG");
-        something.useImplicitXVals();
-        plot.setRangeBoundaries(0, 250, BoundaryMode.FIXED);
-        plot.setDomainBoundaries(0, 20, BoundaryMode.FIXED);
+        //something.useImplicitXVals();
+        plot.setRangeBoundaries(0, 100, BoundaryMode.FIXED);
+        //plot.setDomainBoundaries(0, 20, BoundaryMode.FIXED);
+        LineAndPointFormatter series1Format = new LineAndPointFormatter();
+        series1Format.setPointLabelFormatter(new PointLabelFormatter());
+        series1Format.configure(getApplicationContext(),
+                R.xml.line_point_formatter_with_labels);
 
-        plot.addSeries(something, new LineAndPointFormatter(Color.rgb(0, 0, 1), null, null, null));
-        plot.setDomainStepValue(5);
+        //plot.addSeries(something, new LineAndPointFormatter(Color.rgb(200, 100, 100), null, null, null));
+        plot.addSeries(something,series1Format);
+        // plot.setDomainStepValue(5);
+        //plot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, 5);
         plot.setTicksPerRangeLabel(3);
 
 
@@ -91,98 +103,91 @@ public class Details extends AppCompatActivity implements View.OnClickListener {
         docnum = sharedPreferences.getString("docnumber", "Invalid");
 
 
+        a1.setText("Enable bluetooth");
+        a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!bluetoothAdapter.isEnabled()) {
+                    Intent on = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(on, 5);
+                } else {
+                    Toast.makeText(Details.this, "Already on", Toast.LENGTH_SHORT).show();
+                }
 
-
-
-
-    a1.setText("Enable bluetooth");
-    a1.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (!bluetoothAdapter.isEnabled()) {
-                Intent on = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(on, 5);
-            } else {
-                Toast.makeText(Details.this, "Already on", Toast.LENGTH_SHORT).show();
             }
-
-        }
-    });
+        });
 
 
-    a2.setText("Pair");
-    a2.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            pairedDevice = bluetoothAdapter.getBondedDevices();
-            Log.v("bluetooth","size"+pairedDevice.size());
-            for (BluetoothDevice device : pairedDevice) {
-                Log.v("bluetooth","name: "+device.getName());
-                if (device.getName().equals("EMG-slave")) {
-                    //address = device.getAddress();
-                    Log.v("bluetooth", "address: " + device.getAddress());
-                    Toast.makeText(Details.this, address, Toast.LENGTH_SHORT).show();
-                    bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
-                    Log.v("bluetooth","Remote++++bluetoothDevice: "+bluetoothDevice.getName());
-                    try {
-                        bluetoothSocket = createBluetoothSocket(bluetoothDevice);
-                        Log.v("bluetooth","bluetoothsocket created ");
+        a2.setText("Pair");
+        a2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pairedDevice = bluetoothAdapter.getBondedDevices();
+                Log.v("bluetooth", "size" + pairedDevice.size());
+                for (BluetoothDevice device : pairedDevice) {
+                    Log.v("bluetooth", "name: " + device.getName());
+                    if (device.getName().equals("EMG-slave")) {
+                        //address = device.getAddress();
+                        Log.v("bluetooth", "address: " + device.getAddress());
+                        Toast.makeText(Details.this, address, Toast.LENGTH_SHORT).show();
+                        bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+                        Log.v("bluetooth", "Remote++++bluetoothDevice: " + bluetoothDevice.getName());
+                        try {
+                            bluetoothSocket = createBluetoothSocket(bluetoothDevice);
+                            Log.v("bluetooth", "bluetoothsocket created ");
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            bluetoothSocket.connect();
+                            Log.v("bluetooth", "bluetoothsocket connected");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        connectedThread = new ConnectedThread(bluetoothSocket);
+                        connectedThread.start();
+                        //Toast.makeText(Details.this, "Connect success!", Toast.LENGTH_SHORT).show();
+
                     }
-                    try {
-                        bluetoothSocket.connect();
-                        Log.v("bluetooth", "bluetoothsocket connected");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    connectedThread = new ConnectedThread(bluetoothSocket);
-                    connectedThread.start();
-                    //Toast.makeText(Details.this, "Connect success!", Toast.LENGTH_SHORT).show();
-
+                    //connectedThread = new ConnectedThread(bluetoothSocket);
+                    //connectedThread.start();
                 }
                 //connectedThread = new ConnectedThread(bluetoothSocket);
                 //connectedThread.start();
+
+
             }
-            //connectedThread = new ConnectedThread(bluetoothSocket);
-            //connectedThread.start();
+        });
 
 
+        a3.setText("Send A");
+        a3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connectedThread.write("A");
 
-
-
-        }
-    });
-
-
-    a3.setText("Send A");
-    a3.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            connectedThread.write("A");
-
-        }
-    });
-
-    a4.setText("Send S");
-    a4.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            connectedThread.write("S");
-
-        }
-    });
-
-    bluetoothIn = new Handler() {
-
-        public void HandleMessage(Message msg) {
-            if (msg.what == HandlerState) {
             }
-        }
+        });
 
-    };
+        a4.setText("Send S");
+        a4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connectedThread.write("S");
+
+            }
+        });
+
+        bluetoothIn = new Handler() {
+
+            public void HandleMessage(Message msg) {
+                if (msg.what == HandlerState) {
+                }
+            }
+
+        };
 
 
     }
@@ -192,6 +197,7 @@ public class Details extends AppCompatActivity implements View.OnClickListener {
         return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
         //creates secure outgoing connecetion with BT device using UUID
     }
+
     @Override
     public void onClick(View v) {
 
@@ -231,6 +237,7 @@ public class Details extends AppCompatActivity implements View.OnClickListener {
         final byte delimiter = 10;
         byte[] readBuffer;
         int readBufferPosition;
+
         public ConnectedThread(BluetoothSocket bluetoothSocket1) {
             try {
                 inputStream = bluetoothSocket1.getInputStream();
@@ -238,68 +245,70 @@ public class Details extends AppCompatActivity implements View.OnClickListener {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            try
-            {
+            try {
                 File root = new File(Environment.getExternalStorageDirectory(), "EEG");
                 if (!root.exists()) {
                     root.mkdirs();
                 }
                 File gpxfile = new File(root, "eeg.txt");
                 writer = new FileWriter(gpxfile);
-                writer.append("PFA report of "+myname+"\n");
+                writer.append("PFA report of " + myname + "\n");
 //                writer.flush();
 //                writer.close();
 //                Toast.makeText(MainActivity4.this, "Saved", Toast.LENGTH_SHORT).show();
-            }
-            catch(IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
         }
 
         public void run() {
+
+            int counter = 0;
             readBuffer = new byte[1024];
             readBufferPosition = 0;
             //byte[] buffer = new byte[1024];
             int bytes;
+            MyIndexFormat mif = new MyIndexFormat ();
             while (true) {
                 try {
 
-                   // bytes = inputStream.read(buffer);
-                    bytes=inputStream.available();
+                    // bytes = inputStream.read(buffer);
+                    bytes = inputStream.available();
                     //Log.d("tag","bytes: " + bytes);
 
                     //String readMessage = new String(buffer, 0, bytes);
-                    if(bytes>0)
-                    {
+                    if (bytes > 0) {
                         byte[] buffer = new byte[bytes];
                         inputStream.read(buffer);
-                        for(int i=0;i<bytes;i++)
-                        {
-                            byte b=buffer[i];
-                            if(b==delimiter)
-                            {
-                                byte[] encodedBytes = new byte[readBufferPosition-1];
+                        for (int i = 0; i < bytes; i++) {
+                            byte b = buffer[i];
+                            if (b == delimiter) {
+                                byte[] encodedBytes = new byte[readBufferPosition - 1];
                                 System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
                                 final String data = new String(encodedBytes, "US-ASCII");
                                 readBufferPosition = 0;
-                                int xyz=Integer.parseInt(data);
-
-                                writer.append(xyz+" ");
+                                int xyz = Integer.parseInt(data);
+                                counter += 5;
+                                Log.v("tag", "counter: " + counter);
+                                writer.append(xyz + " ");
                                 if (something.size() > max_size) {
                                     something.removeFirst();
                                 }
-                                something.addLast(null, xyz);
+                                //something.addLast(null, xyz);
+                                something.addLast(counter,xyz);
                                 //Log.d("tag", "readMessage" +Integer.parseInt(readMessage));
                                 //omething.addLast(null, Integer.parseInt(readMessage));
+                                //String[] mylable=new String [10];
+                                //mylable[0]="10";
+                                //mif.Labels =mylable;
+                                //plot.getGraphWidget().setDomainValueFormat(mif);
+
                                 plot.redraw();
                                 bluetoothIn.obtainMessage(HandlerState, bytes, -1, data).sendToTarget();
                                 write("*");
 
-                            }
-                            else
-                            {
+                            } else {
 
                                 readBuffer[readBufferPosition++] = b;
                             }
@@ -310,31 +319,25 @@ public class Details extends AppCompatActivity implements View.OnClickListener {
                     //String temp="";
                     //for (int i=0;i<bytes;i++)
                     //{
-                        //temp+=buffer[i];
+                    //temp+=buffer[i];
                     //}
                     //Log.d("tag","data: "+data);
                     //int xyz = buffer[0];
                     //int xyz=Integer.parseInt(readMessage);
-                   // Log.d("tag", readMessage + " xyz" + xyz);
+                    // Log.d("tag", readMessage + " xyz" + xyz);
                     //writer.append(xyz+" ");
 //                    writer.flush();
 //                    writer.close();
 
 
-
-
-
-
-
                     //if (something.size() > max_size) {
-                        //something.removeFirst();
+                    //something.removeFirst();
                     //}
-                   // something.addLast(null, xyz);
+                    // something.addLast(null, xyz);
                     //Log.d("tag", "readMessage" +Integer.parseInt(readMessage));
                     //omething.addLast(null, Integer.parseInt(readMessage));
-                   // plot.redraw();
-                   // bluetoothIn.obtainMessage(HandlerState, bytes, -1, readMessage).sendToTarget();
-
+                    // plot.redraw();
+                    // bluetoothIn.obtainMessage(HandlerState, bytes, -1, readMessage).sendToTarget();
 
 
                 } catch (IOException e) {
@@ -349,7 +352,7 @@ public class Details extends AppCompatActivity implements View.OnClickListener {
             try {
                 outputStream.write(msg);
                 //outputStream.write();
-                if (input.equals("S")){
+                if (input.equals("S")) {
                     writer.flush();
                     writer.close();
 
@@ -361,5 +364,31 @@ public class Details extends AppCompatActivity implements View.OnClickListener {
             }
         }
 
+    }
+
+    public class MyIndexFormat extends Format {
+
+        public String[] Labels = null;
+
+        @Override
+        public StringBuffer format(Object obj,
+                                   StringBuffer toAppendTo,
+                                   FieldPosition pos) {
+
+            // try turning value to index because it comes from indexes
+            // but if is too far from index, ignore it - it is a tick between indexes
+            float fl = ((Number) obj).floatValue();
+            int index = Math.round(fl);
+            if (Labels == null || Labels.length <= index ||
+                    Math.abs(fl - index) > 0.1)
+                return new StringBuffer("");
+
+            return new StringBuffer(Labels[index]);
+        }
+
+        @Override
+        public Object parseObject(String string, ParsePosition position) {
+            return null;
+        }
     }
 }
